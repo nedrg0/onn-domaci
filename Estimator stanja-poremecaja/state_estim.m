4d39dc6c6ee2c48c0114ebb7ba9f9bcd82d18167
nedrg0 <predrag.nuzdic.github@gmail.com>
2026-02-03 10:22:31 +0100

    est

--- Estimator stanja-poremecaja/state_estim.m
+++ Estimator stanja-poremecaja/state_estim.m
@@ -8,36 +8,9 @@ sys = ss(A, B, C, D);
 %initial(sys, [1; 1; 1])         %Sopstveni odziv
 %pause;
 T = 1e-3;
-%% Regulator
 
-Ts = 5;
-Ksi = 0.7;
-w0 = 5/ ( Ts * Ksi);
-
-p = [-10, -Ksi * w0 + 1i * w0 * sqrt(1 - Ksi^2), -Ksi * w0 - 1i * w0 * sqrt(1 - Ksi^2)];               %Zeljeni polovi
-%p = [-2, -2, -2];
-fd = poly(p);
-
-K = acker(A, B, p);     % ili samo p
-%K = [100, 100, 0]
-%initial(sys, [1; 1; 1])
+%% Estimator
+L = acker(transpose(A), transpose(C), [-2, -2, -2]);
 
 
-%% Zatvorena povratna sprega [1. DEO]
-s = tf('s');
-Gcl =  C * inv(s*eye(size(A)) - (A - B*K)) * B + D;
-Gcl_2ord = tf([0 dcgain(Gcl)], poly([-Ksi * w0 + 1i * w0 * sqrt(1 - Ksi^2), -Ksi * w0 - 1i * w0 * sqrt(1 - Ksi^2)]));
-
-%Provera polova
-p_cl = pole(Gcl);
-
-[~ , i] = min(abs(real(p_cl)));
-dom_poles = p_cl(i, 1);
-
-tau = 1/abs(real(dom_poles));
-Ts_cl = 5 * tau;
-Ksi_cl = abs(real(dom_poles)) / abs(dom_poles);
-%% Saturacija [3. Deo]
-umax = 1;
-umin = -1;
 
